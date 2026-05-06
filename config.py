685d46df8d74917eb93d728e491ff622469bc2b9
Omar Albogdady <Omar@Abds-Mac-mini.local>
2026-05-06 21:21:33 +0400

    Fix HuggingFace URLs

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 MODELS = {
     "phi": {
         "provider": "huggingface",
-        "url": "https://api-inference.huggingface.co/microsoft/Phi-3.5-mini-instruct"
+        "url": "https://api-inference.huggingface.co/models/microsoft/Phi-3.5-mini-instruct"
     },
     "llama3": {
         "provider": "huggingface",
-        "url": "https://api-inference.huggingface.co/meta-llama/Llama-3.1-8B-Instruct"
+        "url": "https://api-inference.huggingface.co/models/meta-llama/Llama-3.1-8B-Instruct"
     },
     "gemma": {
         "provider": "huggingface",
-        "url": "https://api-inference.huggingface.co/google/gemma-2-9b-it"
+        "url": "https://api-inference.huggingface.co/models/google/gemma-2-9b-it"
     },
     "groq_llama3": {
         "provider": "groq",
